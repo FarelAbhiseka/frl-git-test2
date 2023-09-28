214097542f5592c4f392494f5f91bdbc39c4ec05
Farel Abhiseka <farelabhiseka1927@gmail.com>
2023-09-28 18:11:39 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
     
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, voluptates.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 
 </body>
 </html>
